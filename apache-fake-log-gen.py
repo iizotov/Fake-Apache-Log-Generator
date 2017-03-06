64b003c911db68d84d3291d5bc4d3d1be67af1f8
Igor Izotov <igor.izotov@microsoft.com>
2017-03-06 17:34:26 +1100

    Changed CSV to PSV (pipe-delimited) + added a generated sample

--- apache-fake-log-gen.py
+++ apache-fake-log-gen.py
@@ -119,7 +119,7 @@ while (flag):
 	useragent = numpy.random.choice(ualist,p=[0.5,0.3,0.1,0.05,0.05] )()
 
 
-	f.write('%s,-,-,[%s %s],"%s %s HTTP/1.0",%s,%s,"%s","%s"\n' % (ip,dt,tz,vrb,uri,resp,byt,referer,useragent))
+	f.write('%s|-|-|%s %s|%s %s HTTP/1.0|%s|%s|%s|%s\n' % (ip,dt,tz,vrb,uri,resp,byt,referer,useragent))
 
 	log_lines = log_lines - 1
 	flag = False if log_lines == 0 else True
